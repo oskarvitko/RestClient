0aac1d8d8389f313c754db15f1d2d7354b18eeea
oskarvitko16@gmail.com <oskarvitko16@gmail.com>
2021-05-13 20:11:38 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import 'materialize-css'
+import {BrowserRouter as Router} from 'react-router-dom'
+import { AuthContext } from './context/AuthContext';
+import { useAuth } from './hooks/auth.hook';
+import { useRoutes } from './routes';
+import {Navbar} from './components/Navbar'
+import {Loader} from './components/Loader'
 
 function App() {
+  const {token, login, logout, userId, ready} = useAuth()
+  const isAuthenticated = !!token
+  const routes = useRoutes(isAuthenticated)
+
+  if (!ready) {
+    return <Loader />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthContext.Provider  value={{
+      token, login, logout, userId, isAuthenticated
+    }}>
+      <Router>
+        { isAuthenticated && <Navbar />}
+        <div className="container">
+          {routes}
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  )
 }
 
 export default App;
